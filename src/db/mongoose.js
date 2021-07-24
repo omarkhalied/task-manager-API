dad8f4468077173c6740cee7f23d7ec261fba47f
omarkhalied <okhalid397@gmail.com>
2021-07-24 17:43:39 +0300

    DB Connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
